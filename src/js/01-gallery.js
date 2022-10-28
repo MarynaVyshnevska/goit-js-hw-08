13a3e5a4dc6bdc54960b975c1e2fca18db6aad01
Maryna Vyshnevska <vyshnevska.maryna@gmail.com>
2022-10-28 14:33:54 +0300

    first

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,43 @@
+import SimpleLightbox from "simplelightbox";
+
+import "simplelightbox/dist/simple-lightbox.min.css";
+
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+
 // Change code below this line
 
-console.log(galleryItems);
+const lightboxConteiner = document.querySelector('.gallery');
+const cardsLightbox = createLightboxImg(galleryItems);
+
+lightboxConteiner.insertAdjacentHTML('afterbegin', cardsLightbox);
+
+// create html lightbox
+function createLightboxImg(galleryItems) {
+    return galleryItems
+        .map(({ preview, original, description }) => {
+            return `
+                <a class="gallery__item" href="${original}">
+                    <img 
+                        class="gallery__image" 
+                        src="${preview}" 
+                        alt="${description}" />
+                </a>
+            `;
+        })
+        .join('');
+
+}
+
+//  Modal window
+let lightbox = new SimpleLightbox('.gallery a', {
+    /* options */
+    captionsData: 'alt',
+    captionDelay: 250,
+    animationSpeed: 200,
+    // nableKeyboard: true defolt -><- + esc
+    // масштаб изображения
+    scaleImageToRatio: true,
+
+});
+
